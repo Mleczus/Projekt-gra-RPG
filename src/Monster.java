3212e13c691a3b5513023938c305d6bab655250a
Qki <jakubmleczkopl@gmail.com>
2025-10-16 15:09:48 +0200

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,7 +1,10 @@
-//import java.util.Random;
-//
-//public class Monster {
-//Random rand = new Random();
-//int ilosc = rand.nextInt(10) + 1;
-//System.out.println("liczba: " + ilosc);
-//}
+public class Monster extends Character {
+    public Monster(String name, int hp, int attackPower) {
+        super(name, hp, attackPower);
+    }
+
+    public void attack(Character target) {
+        System.out.println(name + " atakuje bohatera!");
+        target.takeDamage(attackPower);
+    }
+}
