3b29348e0b6ea1353240b54fe921e41ea4e6a194
Qki <jakubmleczkopl@gmail.com>
2025-10-16 15:09:16 +0200

    Update Mage.java

--- src/Mage.java
+++ src/Mage.java
@@ -1,2 +1,10 @@
-public class Mage {
+public class Mage extends Character implements Attackable {
+    public Mage(String name) {
+        super(name, 60, 20);
+    }
+
+    public void attack(Character target) {
+        System.out.println(name + " rzuca zaklęcie!");
+        target.takeDamage(attackPower);
+    }
 }
