41996b2ebd4217262c1c9f0f2c8dfce5d46170ba
Qki <jakubmleczkopl@gmail.com>
2025-10-16 15:10:03 +0200

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,11 @@
-    public class Boss {
+public class Boss extends Monster {
+    public Boss(String name) {
+        super(name, 150, 25);
     }
+
+    @Override
+    public void attack(Character target) {
+        System.out.println(name + " wykonuje potężny atak!");
+        target.takeDamage(attackPower + 10);
+    }
+}
