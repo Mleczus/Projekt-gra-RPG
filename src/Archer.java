76f842b0420a48e4b8ffd648da13d7c25a724f7c
Qki <jakubmleczkopl@gmail.com>
2025-10-16 15:08:52 +0200

    Update Archer.java

--- src/Archer.java
+++ src/Archer.java
@@ -1,2 +1,10 @@
-public class Archer {
+public class Archer extends Character implements Attackable {
+    public Archer(String name) {
+        super(name, 70, 15);
+    }
+
+    public void attack(Character target) {
+        System.out.println(name + " strzela z łuku!");
+        target.takeDamage(attackPower);
+    }
 }
