35c2e5440817cd37d38262898f9587673db934cb
Qki <jakubmleczkopl@gmail.com>
2025-10-22 00:13:45 +0200

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -1,33 +1,45 @@
 public abstract class Character {
-    protected String name;
+    protected String imie;
     protected int hp;
     protected int level;
-    protected int attackPower;
+    protected int mocataku;
 
-    public Character(String name, int hp, int attackPower) {
-        this.name = name;
+    public Character(String imie, int hp, int level, int mocataku) {
+        this.imie = imie;
         this.hp = hp;
-        this.level = 1;
-        this.attackPower = attackPower;
+        this.level = level;
+        this.mocataku = mocataku;
     }
 
-    public boolean isAlive() {
+    public void levelUp(){
+        level++;
+        hp += 20;
+        mocataku += 15;
+        System.out.println("LEVEL UP NA POZIOM: " + level);
+    }
+
+    public void pokazStatystyki(){
+        System.out.println("STATYSTYKI: | " + imie + " | " + "HP: " + hp + " | " + "LVL: " + level + " | " + "MOC: " + mocataku + " | " );
+    }
+
+    public void dostawaneobrazenia(int obrazenia){
+        hp -= obrazenia;
+        if(hp <= 0){
+            System.out.println("Nie żyjesz. | PRZEGRANA |");
+        }
+    }
+
+    public boolean czyZyje(){
         return hp > 0;
     }
 
-    public void takeDamage(int damage) {
-        hp -= damage;
-        if (hp < 0) hp = 0;
+    public void Leczenie(){
+        hp += 20;
+        mocataku += 2;
+        levelUp();
     }
 
-    public void levelUp() {
-        level++;
-        hp += 10;
-        attackPower += 5;
-        System.out.println(name + " awansował na poziom " + level + "!");
-    }
 
-    public void showStats() {
-        System.out.println("Postać: " + name + " | HP: " + hp + " | Poziom: " + level + " | Atak: " + attackPower);
-    }
+
+    public abstract void attack(Character target); // Abstrakcyjna metoda — każda klasa musi ją zaimplementować
 }
