64e2e9caa976e22f6ecb0fdc2c668b8af6b297ec
Qki <jakubmleczkopl@gmail.com>
2025-10-22 00:12:24 +0200

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,11 +1,10 @@
-public class Boss extends Monster {
-    public Boss(String name) {
-        super(name, 150, 25);
+public class Boss extends Character {
+    public Boss(String imie, int hp, int level, int mocataku){
+        super(imie,hp,level,mocataku);
     }
 
-    @Override
-    public void attack(Character target) {
-        System.out.println(name + " wykonuje potężny atak!");
-        target.takeDamage(attackPower + 10);
+    public void attack(Character target){
+        System.out.println(imie + " zadał potęrzny cios bohaterowi!");
+        target.dostawaneobrazenia(mocataku);
     }
 }
